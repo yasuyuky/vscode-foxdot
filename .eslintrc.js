689e43452a7ac8c627d9e32463ed4c759439e131
yasuyuky <yasuyuki.ymd@gmail.com>
2020-03-15 22:06:43 +0900

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [],
+  ignorePatterns: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/class-name-casing": "warn",
+    "@typescript-eslint/member-delimiter-style": [
+      "warn",
+      {
+        multiline: {
+          delimiter: "semi",
+          requireLast: true
+        },
+        singleline: {
+          delimiter: "semi",
+          requireLast: false
+        }
+      }
+    ],
+    "@typescript-eslint/semi": ["warn", "always"],
+    "eqeqeq": ["warn", "always"],
+    "no-redeclare": "warn",
+    "no-throw-literal": "warn"
+  },
+  settings: {}
+};
